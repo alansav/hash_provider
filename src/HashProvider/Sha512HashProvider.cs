f0f5b8777ddfdf3dcb4f59d9153efff9b0b8dcb6
Alan Savage <alansavage.dev@gmail.com>
2018-11-24 21:10:40 +0000

    Add SHA512 Hash Provider

---
+++ src/HashProvider/Sha512HashProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Savage.Providers
+{
+    public class Sha512HashProvider : IHashProvider
+    {
+        public byte[] ComputeHash(byte[] value)
+        {
+            var alg = System.Security.Cryptography.SHA512.Create();
+            return alg.ComputeHash(value);
+        }
+    }
+}
---
+++ src/UnitTests/Sha512HashProviderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace Savage.Providers
+{
+    public class Sha512HashProviderTests
+    {
+        [Fact]
+        public void ComputeHash_returns_expected_value()
+        {
+            var source = new byte[] { 1, 2, 3 };
+            var expectedHash = Convert.FromBase64String("J4ZMxSGalRp6blK4yN3faYHQmNoWWNliWMhwssiN+8tRhBrqFyoouvpqeXMRZVhGdwZgRclZ7Q+ZKWiNBN78KQ==");
+
+            var sut = new Sha512HashProvider();
+            var actual = sut.ComputeHash(source);
+
+            Assert.Equal(expectedHash, actual);
+        }
+    }
+}
